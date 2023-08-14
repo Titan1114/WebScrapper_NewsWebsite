f83cf027d46cf114b6dfe0a664cecc527c585746
Pratik <rajpratik001@gmail.com>
2023-08-15 00:39:48 +0530

    webScrappingPart

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.businesstoday.in/technology/news"
+req = requests.get(url)
+
+soup = BeautifulSoup(req.content, "html.parser")
+
+outerData = soup.find_all("div", class_="widget-listing", limit=6)
+finalNews=""
+for data in outerData:
+    news = data.div.div.a["title"]
+    finalNews +="\u2022 "+ news + "\n"
+print(finalNews)
 
