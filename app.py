f6270999bb3042fe9cbf80b5a8202c052d7cd154
Pratik <rajpratik001@gmail.com>
2023-08-16 20:47:23 +0530

    frontend

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.businesstoday.in/technology/news"
-req = requests.get(url)
+app= Flask(__name__)
+@app.route('/',methods= ["GET", "POST"])
+def index():
+    url = "https://www.businesstoday.in/technology/news"
+    req = requests.get(url)
 
-soup = BeautifulSoup(req.content, "html.parser")
+    soup = BeautifulSoup(req.content, "html.parser")
 
-outerData = soup.find_all("div", class_="widget-listing", limit=6)
-finalNews=""
-for data in outerData:
-    news = data.div.div.a["title"]
-    finalNews +="\u2022 "+ news + "\n"
-print(finalNews)
-
+    outerData = soup.find_all("div", class_="widget-listing", limit=6)
+    finalNews=""
+    for data in outerData:
+        news = data.div.div.a["title"]
+        finalNews +="\u2022 "+ news + "\n"
+    return render_template("index.html", News=finalNews)
